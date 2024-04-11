960ae96799ddf6cd214aa27fdb24c233f639f2aa
itsq7n <166761932+itsq7n@users.noreply.github.com>
2024-04-11 19:40:51 -0400

    Create BondDuration_File.py

---
+++ BondDuration_File.py
@@ -0,0 +1,14 @@
+
+def getBondDuration(y, face, couponRate, m, ppy = 1):
+    pvcfsum = 0
+    pvcft = 0
+    cf = couponRate * face/ppy
+    for i in range(1,m*ppy+1):
+        pvf = (1 + y/ppy) ** -i
+        pvcf = pvf * cf
+        pvcfsum += pvcf
+        pvcft += pvcf * i
+    pvcfsum += face * (1 + y/ppy) ** -(m * ppy)
+    pvcft += face * (1 + y/ppy) ** -(m * ppy) * m * ppy
+    duration = pvcft / pvcfsum
+    return duration
