622c973dc1b3810ed35c9129aa6ae5841aa40f0c
itsq7n <166761932+itsq7n@users.noreply.github.com>
2024-04-11 19:40:58 -0400

    Create BondPrice_Z_File.py

---
+++ BondPrice_Z_File.py
@@ -0,0 +1,8 @@
+def getBondPrice_Z(face, couponRate, times, yc):
+    pvfsum = 0
+    cf = couponRate * face
+    for t, i in zip(times, yc):
+        pvf = (1 + i) ** - t
+        pvfsum += pvf
+    pvcfsum = pvfsum * cf + face * (1 + yc[-1]) ** -times[-1]
+    return(pvcfsum)
