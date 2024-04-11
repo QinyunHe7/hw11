d853f972562c61f881467fa0895c2e71f91351d4
itsq7n <166761932+itsq7n@users.noreply.github.com>
2024-04-11 19:40:56 -0400

    Create BondPrice_File.py

---
+++ BondPrice_File.py
@@ -0,0 +1,9 @@
+def getBondPrice(y, face, couponRate, m, ppy=1):
+    pvcfsum = 0
+    cf = couponRate * face/ppy
+    for i in range(1, m*ppy+1):
+        pvf = (1 + y/ppy) ** -i
+        pvcf = pvf * cf
+        pvcfsum += pvcf
+    pvcfsum += face * (1 + y/ppy) ** -(m * ppy)
+    return pvcfsum
